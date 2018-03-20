5ab3bac898d869fe46e685f314b22cf3f0af0f28
Pedro Solis Garcia <solisgpedro@gmail.com>
2018-03-20 21:07:32 +0100

    Commented out event emmiter features, replaced now with routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  loadedFeature = 'recipes';
+  // loadedFeature = 'recipes';
 
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-  }
+  // onNavigate(feature: string) {
+  //   this.loadedFeature = feature;
+  // }
 }
